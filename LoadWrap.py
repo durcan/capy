9692d552cc0a6fe440a6ee9fa6a95838186a7cca
Brett Cornell <brett.durcan@gmail.com>
2013-07-18 16:45:34 -0500

    added LoadWrap and __init__

---
+++ LoadWrap.py
@@ -0,0 +1,43 @@
+"""
+LoadWrap is a madule that provides a convience wrapper object around peter's
+Load functions. It is a quick attempt to provide a persistent object that remembers
+what data you have loaded incase you want some more like it.
+"""
+
+import LoadFunction
+import numpy
+
+class data(object):
+    """The class describes some convience methods to access root data
+    with """
+
+    def __init__(self, detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba', data_location = '/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged'):
+        self.min_date = min_date
+        self.max_date = max_date
+        self.which_dataSet = which_dataSet
+        self.data_location =  data_location
+        self.detnum = detnum
+        self.kwargs = {'detnum': detnum, 'min_date': min_date, 'max_date': max_date, 'which_dataSet': which_dataSet, 'data_location': data_location}
+        self.loaded_rrqs = []
+        self.loaded_rqs = []
+        self.loaded_cuts = []
+
+    def rrq(self, rrqs_to_load = [], eventRQ = False):
+        self.kwargs['rrqs_to_load'] = rrqs_to_load
+        for rrq in rrqs_to_load:
+            self.loaded_rrqs.append(rrq)
+        self.kwargs['eventRQ'] = eventRQ
+        return LoadFunction.LoadRRQ(**self.kwargs)
+
+    def rq(self, rqs_to_load = [], eventRQ = False):
+        self.kwargs['rqs_to_load'] = rqs_to_load
+        for rq in rqs_to_load:
+            self.loaded_rqs.append(rq)
+        self.kwargs['eventRQ'] = eventRQ
+        return LoadFunction.LoadRQ(**self.kwargs)
+
+    def cut(self, cuts_to_load=[]):
+        self.kwargs['cuts_to_load'] = cuts_to_load
+        for cut in cuts_to_load:
+            self.loaded_cuts.append(cut)
+        return LoadFunction.LoadCut(**self.kwargs)
---
+++ __init__.py
@@ -0,0 +1,1 @@
+from LoadWrap import data
