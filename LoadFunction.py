ec3b0607411753e4e9927e1c476f0923e0fb21e5
Brett Cornell <brett.durcan@gmail.com>
2013-07-18 16:58:13 -0500

    Fixed directory problem

--- LoadFunction.py
+++ LoadFunction.py
@@ -1,13 +1,13 @@
-#The follwoing file will do in python something similar what is done in
-#matllab + CAP. The basic idea is that you can load .root trees using
-#the functionality provided by the very nice root_numpy package.
+# The follwoing file will do in python something similar what is done in
+# matllab + CAP. The basic idea is that you can load .root trees using
+# the functionality provided by the very nice root_numpy package.
 #
-#Written by Peter Redl. For comments please Email me at: redl@stanford.edu
-#Version 0.0     06/09/2013
+# Written by Peter Redl. For comments please Email me at: redl@stanford.edu
+# Version 0.0     06/09/2013
 #
-#This is written with numpy version 1.7 in mind... if you have an oder
-#numpy version I guarantee nothing, as a matter of fact I guarantee nothing
-#regardless of your numpy version, use at your own risk :)! Have fun...
+# This is written with numpy version 1.7 in mind... if you have an oder
+# numpy version I guarantee nothing, as a matter of fact I guarantee nothing
+# regardless of your numpy version, use at your own risk :)! Have fun...
 
 
 import numpy
@@ -16,22 +16,24 @@ from glob import *
 from root_numpy import *
 import numpy.lib.recfunctions as rfn
 import sys
-def LoadRRQ(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
-            data_location = '/galbadata/R133/dataReleases/Prodv5-3/merged/all',
-            rrqs_to_load = [], eventRQ = False ):
+
+
+def LoadRRQ(detnum, min_date=0, max_date=numpy.inf, which_dataSet='ba',
+            data_location='/galbadata/R133/dataReleases/Prodv5-3/merged/all',
+            rrqs_to_load=[], eventRQ=False):
 
     if not ((which_dataSet is 'ba') or (which_dataSet is 'cf') or
             (which_dataSet is 'bg_permitted')):
-        print 'You have selected a dataset to load that I do not recognize'     
+        print 'You have selected a dataset to load that I do not recognize'
         print 'Please select eighter "ba", "cf" or "bg_permitted"... exiting now'
         sys.exit()
-    
+
     if (detnum < 1100) or (detnum > 1115):
         print 'You have selected a detector number that is outside of the range'
         print 'Now exiting. Select a detector number that is between 1101-1115'
-        #break
+        # break
         sys.exit()
-    
+
     if (min_date == 0) or (max_date == numpy.inf):
         print 'You have elected to load all of the ', which_dataSet, ' data'
     else:
@@ -43,65 +45,77 @@ def LoadRRQ(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
         print 'Minimum load day is: ', min_date, ' Maximum load day is: ', max_date
         min_date = numpy.int(min_date)
         max_date = numpy.int(max_date)
-        
+
     detnum = numpy.int(detnum - 1100)
     print "Loading RRQ's: ", rrqs_to_load
-    #Deciding what data To load....
+    # Deciding what data To load....
     LoadingData = data_location + '/' + which_dataSet
-    AllFiles            = glob(LoadingData + '/*calib_Prodv5-3_01*.root')
-    DataRelease         = 53
+    AllFiles = glob(LoadingData + '/*calib_Prodv5-3_01*.root')
+    DataRelease = 53
     if len(AllFiles) == 0:
-        AllFiles        = glob(LoadingData + '/*calib_Prodv5-2_01*.root')
-        DataRelease     = 52
+        AllFiles = glob(LoadingData + '/*calib_Prodv5-2_01*.root')
+        DataRelease = 52
     AllFiles.sort()
-    PassingFilesToLoad  = []
+    PassingFilesToLoad = []
     for line in AllFiles:
         #FileDate = numpy.int(line.split("_")[-2].split("a")[0])
         if DataRelease == 53:
             FileDate = numpy.int(line.split("_")[-2][0:-1])
         if DataRelease == 52:
-            FileDate = numpy.int(line.split("_")[-2][0:-2]) #- 1000000
+            FileDate = numpy.int(line.split("_")[-2][0:-2])  # - 1000000
         print FileDate, line
         FileDate = FileDate - 10000
         if (FileDate >= min_date) and (FileDate <= max_date):
             PassingFilesToLoad = numpy.append(PassingFilesToLoad, line)
-            #print FileDate, line
-    #print 'These are the files we are loading for RRQs: \n', PassingFilesToLoad
-    
-    #Done deciding what data to load...
+            # print FileDate, line
+    # print 'These are the files we are loading for RRQs: \n',
+    # PassingFilesToLoad
+
+    # Done deciding what data to load...
     #PassingFilesToLoad = numpy.sort(PassingFilesToLoad)
-    #now loading the data.... 
+    # now loading the data....
     #RRQ = root2array(PassingFilesToLoad, 'rrqDir/calibzip1', rrqs_to_load)
-    #now loading the data....
+    # now loading the data....
     if eventRQ == False:
         if len(rrqs_to_load) > 0:
-            RRQ = root2array(PassingFilesToLoad, 'rrqDir/calibzip%0.1d'%detnum, rrqs_to_load)
+            RRQ = root2array(
+                PassingFilesToLoad,
+                'rrqDir/calibzip%0.1d' %
+                detnum,
+                rrqs_to_load)
         else:
-            RRQ = root2array(PassingFilesToLoad, 'rrqDir/calibzip%0.1d'%detnum)
+            RRQ = root2array(
+                PassingFilesToLoad,
+                'rrqDir/calibzip%0.1d' %
+                detnum)
         return RRQ
-    if eventRQ == True:
+    if eventRQ:
         if len(rrqs_to_load) > 0:
-            RRQ = root2array(PassingFilesToLoad, 'rrqDir/calibevent', rrqs_to_load)
+            RRQ = root2array(
+                PassingFilesToLoad,
+                'rrqDir/calibevent',
+                rrqs_to_load)
         else:
             RRQ = root2array(PassingFilesToLoad, 'rrqDir/calibevent')
         return RRQ
 
-def LoadRQ(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
-           data_location = '/galbadata/R133/dataReleases/Prodv5-3/merged/all',
-           rqs_to_load = [], eventRQ = False ):
+
+def LoadRQ(detnum, min_date=0, max_date=numpy.inf, which_dataSet='ba',
+           data_location='/galbadata/R133/dataReleases/Prodv5-3/merged/all',
+           rqs_to_load=[], eventRQ=False):
 
     if not ((which_dataSet is 'ba') or (which_dataSet is 'cf') or
             (which_dataSet is 'bg_permitted')):
-        print 'You have selected a dataset to load that I do not recognize'     
+        print 'You have selected a dataset to load that I do not recognize'
         print 'Please select eighter "ba", "cf" or "bg_permitted"... exiting now'
         sys.exit()
-    
+
     if (detnum < 1100) or (detnum > 1115):
         print 'You have selected a detector number that is outside of the range'
         print 'Now exiting. Select a detector number that is between 1101-1115'
-        #break
+        # break
         sys.exit()
-    
+
     if (min_date == 0) or (max_date == numpy.inf):
         print 'You have elected to load all of the ', which_dataSet, ' data'
     else:
@@ -113,64 +127,69 @@ def LoadRQ(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
         print 'Minimum load day is: ', min_date, ' Maximum load day is: ', max_date
         min_date = numpy.int(min_date)
         max_date = numpy.int(max_date)
-        
+
     detnum = numpy.int(detnum - 1100)
     print "Loading RQ's: ", rqs_to_load
-    #Deciding what data To load....
+    # Deciding what data To load....
     LoadingData = data_location + '/' + which_dataSet
 
-    AllFiles            = glob(LoadingData + '/*merge_Prodv5-3_01*.root')
-    DataRelease         = 53
+    AllFiles = glob(LoadingData + '/*merge_Prodv5-3_01*.root')
+    DataRelease = 53
     if len(AllFiles) == 0:
-        AllFiles        = glob(LoadingData + '/*merge_Prodv5-2_01*.root')
-        DataRelease     = 52
-        
-    PassingFilesToLoad  = []
+        AllFiles = glob(LoadingData + '/*merge_Prodv5-2_01*.root')
+        DataRelease = 52
+
+    PassingFilesToLoad = []
     AllFiles.sort()
     for line in AllFiles:
         #FileDate = numpy.int(line.split("_")[-2].split("a")[0])
         FileDate = numpy.int(line.split("_")[-2][0:-1])
         if DataRelease == 52:
             FileDate = numpy.int(line.split("_")[-2][0:-2])
-        #print FileDate
+        # print FileDate
         FileDate = FileDate - 10000
         if (FileDate >= min_date) and (FileDate <= max_date):
             PassingFilesToLoad = numpy.append(PassingFilesToLoad, line)
-    
-    #print 'These are the files we are loading for RQs: \n', PassingFilesToLoad
-    #print rqs_to_load
-    #Done deciding what data to load...
 
-    #now loading the data....
+    # print 'These are the files we are loading for RQs: \n', PassingFilesToLoad
+    # print rqs_to_load
+    # Done deciding what data to load...
+
+    # now loading the data....
     if eventRQ == False:
         if len(rqs_to_load) > 0:
-            RQ = root2array(PassingFilesToLoad, 'rqDir/zip%0.1d'%detnum, rqs_to_load)
+            RQ = root2array(
+                PassingFilesToLoad,
+                'rqDir/zip%0.1d' %
+                detnum,
+                rqs_to_load)
         else:
-            RQ = root2array(PassingFilesToLoad, 'rqDir/zip%0.1d'%detnum)
+            RQ = root2array(PassingFilesToLoad, 'rqDir/zip%0.1d' % detnum)
         return RQ
-    if eventRQ == True:
+    if eventRQ:
         if len(rqs_to_load) > 0:
             RQ = root2array(PassingFilesToLoad, 'rqDir/eventTree', rqs_to_load)
         else:
             RQ = root2array(PassingFilesToLoad, 'rqDir/eventTree')
         return RQ
 
-def LoadCut(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
-            data_location = '/galbadata/R133/dataReleases/Prodv5-3/merged/cuts/',
-            cuts_to_load = [] ):
+
+def LoadCut(detnum, min_date=0, max_date=numpy.inf, which_dataSet='ba',
+            data_location='/galbadata/R133/dataReleases/Prodv5-3/merged/cuts/',
+            cuts_to_load=[]):
 
     if not ((which_dataSet is 'ba') or (which_dataSet is 'cf') or
             (which_dataSet is 'bg_permitted')):
-        print 'You have selected a dataset to load that I do not recognize'     
+        print 'You have selected a dataset to load that I do not recognize'
         print 'Please select eighter "ba", "cf" or "bg_permitted"... exiting now'
         sys.exit()
-    
+
     if (detnum < 1100) or (detnum > 1115):
         print 'You have selected a detector number that is outside of the range'
         print 'Now exiting. Select a detector number that is between 1101-1115'
-        #break
+        # break
         sys.exit()
-    
+
     if (min_date == 0) or (max_date == numpy.inf):
         print 'You have elected to load all of the ', which_dataSet, ' data'
     else:
@@ -183,64 +202,76 @@ def LoadCut(detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba',
         min_date = numpy.int(min_date)
         max_date = numpy.int(max_date)
 
-        
     detnum = numpy.int(detnum - 1100)
-    
-    #Deciding what data To load....
+
+    # Deciding what data To load....
     LoadingData = data_location + '/' + which_dataSet
-    #print LoadingData
-    #sys.exit()
+    # print LoadingData
+    # sys.exit()
     #cuts = numpy.zeros(len(cuts_to_load))
     print LoadingData
-    #exit()
+    # exit()
     cuts = []
     PassingFilesToLoad = []
-    #print LoadingData
+    # print LoadingData
     for i in cuts_to_load:
         print "Loading Cut ", i
-        AllFiles            = []
-        AllFiles            = glob(LoadingData + '/' + i + '/*v53_01*.root')
-        AllFiles.sort()
-        #print LoadingData + '/current/' + i + '*.root'
-        #print AllFiles
-        #sys.exit()
+        AllFiles = []
+        AllFiles = sorted(
+            glob(LoadingData + '/' + i + '/*v53_01*.root'))
+        # print LoadingData + '/current/' + i + '*.root'
+        # print AllFiles
+        # sys.exit()
         for line in AllFiles:
             FileDate = numpy.int(line.split("_")[-2][0:-1])
-            #print FileDate
+            # print FileDate
             FileDate = numpy.int(FileDate - 10000)
-            #print FileDate
-            
-            
+            # print FileDate
+
             if (FileDate >= min_date) and (FileDate <= max_date):
 
                 PassingFilesToLoad = numpy.append(PassingFilesToLoad, line)
                 print FileDate, line
-        #print PassingFilesToLoad
+        # print PassingFilesToLoad
         if len(cuts_to_load) > 1:
-            cuts = rfn.merge_arrays([cuts,root2array(PassingFilesToLoad, 'cutDir/cutzip%0.1d'%detnum, [i])] , flatten = True, usemask = False)
+            cuts = rfn.merge_arrays(
+                [cuts,
+                 root2array(PassingFilesToLoad,
+                            'cutDir/cutzip%0.1d' % detnum,
+                            [i])],
+                flatten=True,
+                usemask=False)
         else:
             print i, detnum
-            cuts = root2array(PassingFilesToLoad, 'cutDir/cutzip%0.1d'%detnum, [i])
+            cuts = root2array(
+                PassingFilesToLoad, 'cutDir/cutzip%0.1d' %
+                detnum, [i])
 
         #cuts = root2array(PassingFilesToLoad, 'cutDir/cutzip%0.1d'%detnum)
         #cuts = numpy.append(cuts, root2array(PassingFilesToLoad, 'cutDir/cutzip%0.1d'%detnum))
         PassingFilesToLoad = []
-                
+
     if len(AllFiles) == -11111111:
-        for i in cuts_to_load:    
-            AllFiles        = glob(LoadingData + '/' + i + '/*.root')
-            
+        for i in cuts_to_load:
+            AllFiles = glob(LoadingData + '/' + i + '/*.root')
+
             for line in AllFiles:
                 FileDate = numpy.int(line.split("_")[-2][0:-2])
                 print FileDate
                 FileDate = FileDate - 10000
                 if (FileDate >= min_date) and (FileDate <= max_date):
                     PassingFilesToLoad = numpy.append(PassingFilesToLoad, line)
-            cuts = numpy.append_fields(cuts, i , root2array(numpy.sort(PassingFilesToLoad), 'cutDir/cutzip%0.1d'%detnum))
+            cuts = numpy.append_fields(
+                cuts,
+                i,
+                root2array(
+                    numpy.sort(
+                        PassingFilesToLoad),
+                    'cutDir/cutzip%0.1d' %
+                    detnum))
             PassingFilesToLoad = []
-            
-        
-    PassingFilesToLoad  = []
-    
-    #now loading the data....
+
+    PassingFilesToLoad = []
+
+    # now loading the data....
     return cuts
--- LoadWrap.py
+++ LoadWrap.py
@@ -4,40 +4,55 @@ Load functions. It is a quick attempt to provide a persistent object that rememb
 what data you have loaded incase you want some more like it.
 """
 
-import LoadFunction
+from . import LoadFunction
 import numpy
 
+
 class data(object):
+
     """The class describes some convience methods to access root data
     with """
 
-    def __init__(self, detnum, min_date = 0 , max_date = numpy.inf, which_dataSet = 'ba', data_location = '/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged'):
+    def __init__(
+        self, detnum, min_date=0, max_date=numpy.inf, which_dataSet='ba',
+            data_location='/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged'):
         self.min_date = min_date
         self.max_date = max_date
         self.which_dataSet = which_dataSet
-        self.data_location =  data_location
+        self.data_location = data_location
         self.detnum = detnum
-        self.kwargs = {'detnum': detnum, 'min_date': min_date, 'max_date': max_date, 'which_dataSet': which_dataSet, 'data_location': data_location}
+        self.kwargs = {
+            'detnum': detnum,
+            'min_date': min_date,
+            'max_date': max_date,
+            'which_dataSet': which_dataSet,
+            'data_location': data_location}
         self.loaded_rrqs = []
         self.loaded_rqs = []
         self.loaded_cuts = []
 
-    def rrq(self, rrqs_to_load = [], eventRQ = False):
+    def rrq(self, rrqs_to_load=[], eventRQ=False):
         self.kwargs['rrqs_to_load'] = rrqs_to_load
         for rrq in rrqs_to_load:
             self.loaded_rrqs.append(rrq)
         self.kwargs['eventRQ'] = eventRQ
-        return LoadFunction.LoadRRQ(**self.kwargs)
+        kwargs = self.kwargs
+        kwargs['data_location'] = self.data_location + '/all'
+        return LoadFunction.LoadRRQ(**kwargs)
 
-    def rq(self, rqs_to_load = [], eventRQ = False):
+    def rq(self, rqs_to_load=[], eventRQ=False):
         self.kwargs['rqs_to_load'] = rqs_to_load
         for rq in rqs_to_load:
             self.loaded_rqs.append(rq)
         self.kwargs['eventRQ'] = eventRQ
-        return LoadFunction.LoadRQ(**self.kwargs)
+        kwargs = self.kwargs
+        kwargs['data_location'] = self.data_location + '/all'
+        return LoadFunction.LoadRQ(**kwargs)
 
     def cut(self, cuts_to_load=[]):
         self.kwargs['cuts_to_load'] = cuts_to_load
         for cut in cuts_to_load:
             self.loaded_cuts.append(cut)
-        return LoadFunction.LoadCut(**self.kwargs)
+        kwargs = self.kwargs
+        kwargs['data_location'] = self.data_location + '/cuts'
+        return LoadFunction.LoadCut(**kwargs)
